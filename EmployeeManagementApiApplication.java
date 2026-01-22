6b62aa28249658c73f9cae8073a0da9c9df09546
PravinBijewar01 <bijewarpravin94@gmail.com>
2026-01-22 17:31:13 +0530

    main class

---
+++ EmployeeManagementApiApplication.java
@@ -0,0 +1,15 @@
+package com.uk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagementApiApplication.class, args);
+
+		System.out.println("start employee manegment portal");
+	}
+
+}
